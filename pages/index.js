9a988a2ac5f5be52ae997eb2562f3892f225c0ab
cbcruk <cbcruk@gmail.com>
2021-10-04 14:12:07 +0900

    :star: feature: ssr로 변경

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,38 @@
-import useSWR from 'swr'
-import { App, Released, Quote, Author, Loading } from '../components'
+import { App, Released, Quote, Author } from '../components'
 
-function Home() {
-  const { data = { isPending: true } } = useSWR('/api/weekly', {
-    revalidateOnFocus: false,
-  })
-
+function Home({ data }) {
   return (
-    <>
-      <Loading isPending={data.isPending} />
-      <App total={data.length ?? 0} isPending={data.isPending}>
-        {({ page }) => {
-          const item = data[page]
+    <App total={data.length ?? 0} isPending={data.isPending}>
+      {({ page }) => {
+        const item = data[page]
 
-          if (!item) {
-            return null
-          }
+        if (!item) {
+          return null
+        }
 
-          return (
-            <>
-              <Released created={item.created_at} released={item.released} />
-              <Quote isLong={item.quote && item.quote.length > 100}>
-                {item.quote}
-              </Quote>
-              <Author author={item.author} />
-            </>
-          )
-        }}
-      </App>
-    </>
+        return (
+          <>
+            <Released created={item.created_at} released={item.released} />
+            <Quote isLong={item.quote && item.quote.length > 100}>
+              {item.quote}
+            </Quote>
+            <Author author={item.author} />
+          </>
+        )
+      }}
+    </App>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://zitat.vercel.app/api/weekly')
+  const data = await response.json()
+
+  return {
+    props: {
+      data,
+    },
+  }
 }
 
 Home.title = 'zitat'
