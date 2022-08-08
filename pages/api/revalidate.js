9409dc6528ee6dbdf4817bd87597d8a2a643ae0d
Eunsoo Lee <cbcruk@users.noreply.github.com>
2022-08-08 22:03:27 +0900

    feat: revalidate 추가

---
+++ pages/api/revalidate.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('next').NextApiHandler}
+ */
+async function handler(req, res) {
+  if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  try {
+    await res.revalidate('/')
+    return res.json({ revalidated: true })
+  } catch (err) {
+    return res.status(500).send('Error revalidating')
+  }
+}
+
+export default handler
