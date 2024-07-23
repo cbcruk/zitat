470738886b619ecdb1ae0d4bf3108dcfd219b4eb
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-07-24 08:46:40 +0900

    fix: 데이터 타입이 Array가 아닐 경우 방어코드 추가

--- lib/sheet.ts
+++ lib/sheet.ts
@@ -4,8 +4,13 @@ import { TodayItem } from './types'
 
 export async function weekly() {
   const rawData = await getResult()
-  const data: TodayItem[] = JSON.parse(rawData || '')
-  const transformData = data.map((item) => {
+  const data = JSON.parse(rawData || '')
+
+  if (!Array.isArray(data)) {
+    return []
+  }
+
+  const transformData = (data as TodayItem[]).map((item) => {
     return {
       ...item,
       created_at_text: getFormattedDate(item.created_at),
