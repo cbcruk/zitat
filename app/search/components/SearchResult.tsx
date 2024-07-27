b016a29122e74149676a524e1af60860e2053b28
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-07-27 23:16:02 +0900

    fix: 검색결과 line-clamp 처리 추가

--- app/search/components/SearchResult.tsx
+++ app/search/components/SearchResult.tsx
@@ -16,7 +16,10 @@ export function SearchResult({ data }: Props) {
             className="py-[10px] first:pt-0 border-[var(--md-sys-color-outline)] break-keep whitespace-pre-line"
           >
             <Link href={`/list/${uuid}`}>
-              <p dangerouslySetInnerHTML={{ __html: quote }} />
+              <p
+                className="line-clamp-3"
+                dangerouslySetInnerHTML={{ __html: quote }}
+              />
               <p
                 className="mt-[4px] text-[12px]"
                 dangerouslySetInnerHTML={{ __html: author }}
