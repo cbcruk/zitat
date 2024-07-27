54cdbbd7a13be0a563a0feb95f162802fe4cbea2
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-07-27 23:07:17 +0900

    fix: next 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/search': ['./zitat.db'],
+    },
+  },
+}
 
 export default nextConfig
