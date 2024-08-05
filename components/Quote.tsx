9d49dde7895d61477bd960de6b08fdadeeb4ec0b
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-08-05 21:46:22 +0900

    fix: 문단 줄바꿈 처리 추가

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -5,7 +5,7 @@ type Props = ComponentProps<'p'>
 function Quote({ children, ...props }: Props) {
   return (
     <p
-      className='text-[2.618rem] data-[is-long="true"]:text-[1.618rem] text-balance break-keep overflow-hidden text-ellipsis'
+      className='text-[2.618rem] data-[is-long="true"]:text-[1.618rem] text-balance break-keep overflow-hidden text-ellipsis whitespace-pre-wrap'
       {...props}
     >
       {children}
