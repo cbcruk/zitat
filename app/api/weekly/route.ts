63240ddf5794278a5414427702cc7ea7440f1c12
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-02-17 19:33:57 +0900

    fix: redis 업데이트 후 revalidatePath 실행 추가

--- app/api/weekly/route.ts
+++ app/api/weekly/route.ts
@@ -1,5 +1,6 @@
 import { headers } from 'next/headers'
 import redis, { ZITAT_WEEKLY } from '../../../lib/redis'
+import { revalidatePath } from 'next/cache'
 
 export async function POST(request: Request) {
   const headersList = headers()
@@ -8,6 +9,9 @@ export async function POST(request: Request) {
 
   if (id === process.env.SCRIPT_ID) {
     await redis.set(ZITAT_WEEKLY, JSON.stringify(body))
+
+    revalidatePath('/', 'page')
+    revalidatePath('/list', 'layout')
 
     return new Response('성공적으로 업데이트되었습니다.', {
       status: 200,
