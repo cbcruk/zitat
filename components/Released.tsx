11c5833f75b00bb7d88c737bcc49b7bd1eeffe7e
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-02-22 09:10:01 +0900

    fix: 오늘 날짜 텍스트 제거 (#15)

--- components/Released.tsx
+++ components/Released.tsx
@@ -1,7 +1,6 @@
 import { ComponentProps } from 'react'
-import { getFormattedDate } from '../utils'
-import { TodayItemSchema } from '../schema/item'
 import { SelectQuoteSchema } from '../lib/db/schema'
+import { getFormattedDate } from '../utils'
 
 type Props = ComponentProps<'div'> & {
   released: SelectQuoteSchema['date']
@@ -10,11 +9,10 @@ type Props = ComponentProps<'div'> & {
 function Released({ released, children }: Props) {
   return (
     <div className="flex justify-between">
-      <p
-        className="flex items-center text-[var(--md-sys-color-tertiary)]"
-        title={released}
-      >
-        {getFormattedDate(new Date())}
+      <p className="flex items-center text-[var(--md-sys-color-tertiary)]">
+        <time dateTime={new Date(released).toISOString()}>
+          {getFormattedDate(released)}
+        </time>
       </p>
       {children}
     </div>
